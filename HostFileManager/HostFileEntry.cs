6a627168f75892b2f490578666e649893a0318b2
sidsoft <sidsoft2014@outlook.com>
2019-06-09 13:07:29 +0100

    Add project files.

---
+++ HostFileManager/HostFileEntry.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel;
+using System.Net;
+
+namespace HostFileManager
+{
+    public class HostFileEntry : INotifyPropertyChanged
+    {
+        private IPAddress iPAddress;
+        private string domain;
+        private bool isActive;
+
+        public HostFileEntry(IPAddress ip, string domain)
+        {
+            IPAddress = ip;
+            Domain = domain;
+        }
+        public HostFileEntry(IPAddress ip, string domain, bool isActive)
+            : this(ip, domain)
+        {
+            IsActive = isActive;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IPAddress IPAddress
+        {
+            get => iPAddress;
+            private set
+            {
+                if (iPAddress != value)
+                {
+                    iPAddress = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IPAddress)));
+                }
+            }
+        }
+        public string Domain
+        {
+            get => domain;
+            set
+            {
+                if (domain != value)
+                {
+                    domain = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Domain)));
+                }
+            }
+        }
+        public bool IsActive
+        {
+            get => isActive;
+            set
+            {
+                if (isActive != value)
+                {
+                    isActive = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsActive)));
+                }
+            }
+        }
+
+        public string IPString
+        {
+            get => IPAddress?.ToString();
+            set
+            {
+                if (IPAddress.TryParse(value, out IPAddress ip))
+                {
+                    IPAddress = ip;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IPAddress)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IPString)));
+                }
+            }
+        }
+    }
+}
---
+++ HostFileManager/MainWindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostFileManager
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private HostFileService _fileService = new HostFileService();
+        private HostFileEntry selectedHostFileEntry;
+        private string _statusMessage;
+
+        public MainWindowViewModel()
+        {
+            HostFileEntries = new ObservableCollection<HostFileEntry>(_fileService.GetHostFileEntries());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<HostFileEntry> HostFileEntries { get; set; }
+        public HostFileEntry SelectedHostFileEntry
+        {
+            get => selectedHostFileEntry;
+            set
+            {
+                if (value != selectedHostFileEntry)
+                {
+                    selectedHostFileEntry = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedHostFileEntry)));
+                }
+            }
+        }
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            private set
+            {
+                if (value != _statusMessage)
+                {
+                    _statusMessage = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusMessage)));
+                }
+            }
+        }
+
+        public void AddEntry()
+        {
+            HostFileEntries.Add(new HostFileEntry(new System.Net.IPAddress(16777343), "change.me", true));
+            SelectedHostFileEntry = HostFileEntries.Last();
+            StatusMessage = "New host added";
+            QueueStatusReset();
+        }
+
+        public void RemoveEntry()
+        {
+            if (SelectedHostFileEntry is null)
+            {
+                StatusMessage = "No host selected to remove";
+                return;
+            }
+
+            if (HostFileEntries.Remove(SelectedHostFileEntry))
+            {
+                SelectedHostFileEntry = null;
+                StatusMessage = "Host removed";
+            }
+            else
+            {
+                StatusMessage = "Could not remove host";
+            }
+            QueueStatusReset();
+        }
+
+        public void Write()
+        {
+            StatusMessage = _fileService.Write(HostFileEntries) ? "File updated" : "Error updating file";
+            QueueStatusReset();
+        }
+
+        private void QueueStatusReset()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(1250);
+                StatusMessage = "";
+            });
+        }
+    }
+}
